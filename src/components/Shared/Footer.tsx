dae1127c26bcf362fc95a19e1820904c88032684
pervej0 <mdparvez222khan@gmail.com>
2024-04-30 02:21:36 +0600

    footer added

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -1,13 +1,17 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
-import Link from "next/link";
 import React from "react";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import facebookIcon from "@/assets/landing_page/facebook.png";
+import instagramIcon from "@/assets/landing_page/instagram.png";
+import twitterIcon from "@/assets/landing_page/twitter.png";
+import linkedIcon from "@/assets/landing_page/linkedin.png";
 
 const Footer = () => {
   return (
-    <Box bgcolor="#2E4053">
+    <Box bgcolor="#273746" py={5}>
       <Container>
         <Stack
-          py={4}
           direction="row"
           justifyContent="center"
           alignItems="center"
@@ -20,6 +24,40 @@ const Footer = () => {
           <Typography color="#ffffff">Medicine</Typography>
           <Typography color="#ffffff">Diagnostics</Typography>
           <Typography color="#ffffff">NGOs</Typography>
+        </Stack>
+        <Stack direction="row" gap={2} justifyContent="center" py={3}>
+          <Image src={facebookIcon} width={30} height={30} alt="facebook" />
+          <Image src={instagramIcon} width={30} height={30} alt="facebook" />
+          <Image src={twitterIcon} width={30} height={30} alt="facebook" />
+          <Image src={linkedIcon} width={30} height={30} alt="facebook" />
+        </Stack>
+        <div className="border-b-[1px] border-dashed"></div>
+        <Stack
+          direction="row"
+          gap={2}
+          justifyContent="space-between"
+          alignItems="center"
+          py={3}
+        >
+          <Typography component="p" color="white">
+            &copy;2024 Ph HealthCare. All Rights Reserved.
+          </Typography>
+          <Typography
+            variant="h5"
+            component={Link}
+            href="/"
+            fontWeight={600}
+            color="white"
+          >
+            P
+            <Box component="span" color="primary.main">
+              H
+            </Box>{" "}
+            Health Care
+          </Typography>
+          <Typography component="p" color="white">
+            Privacy Policy! Terms & Conditions
+          </Typography>
         </Stack>
       </Container>
     </Box>
