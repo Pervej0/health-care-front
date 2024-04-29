964bf42a1f8900f8a5c21eee30649f776ae953f1
pervej0 <mdparvez222khan@gmail.com>
2024-04-30 01:54:36 +0600

    navbar added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <Typography variant="h1" component="h1" textAlign="center">
+        Page Not Found
+      </Typography>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -1,13 +1,35 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
     <Container>
-      <Stack direction="row">
-        <Typography variant="h5" component="h5">
-          P<Box component="span">H</Box> Health Care
+      <Stack py={3} direction="row" justifyContent="space-between">
+        <Typography variant="h5" component="h5" fontWeight={600}>
+          P
+          <Box component="span" color="primary.main">
+            H{" "}
+          </Box>
+          Health Care
         </Typography>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={4}
+        >
+          <Typography component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography>Health Plans</Typography>
+          <Typography>Medicine</Typography>
+          <Typography>Diagnostics</Typography>
+          <Typography>NGOs</Typography>
+        </Stack>
+        <Button LinkComponent={Link} href="/login">
+          Login
+        </Button>
       </Stack>
     </Container>
   );
