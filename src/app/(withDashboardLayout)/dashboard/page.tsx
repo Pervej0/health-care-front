7993108ecaa8477b639e8d6e8fe9d67d3ada9675
pervej0 <mdparvez222khan@gmail.com>
2024-05-07 00:23:20 +0600

    sidebar modified

---
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const page = () => {
+  return <Box mt={10}>Welcome To Dashboard</Box>;
+};
+
+export default page;
--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -1,7 +1,9 @@
+import DashboardDrawer from "@/components/dashboard/DashboardDrawer";
+import { Box } from "@mui/material";
 import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
+  return <DashboardDrawer>{children}</DashboardDrawer>;
 };
 
 export default layout;
