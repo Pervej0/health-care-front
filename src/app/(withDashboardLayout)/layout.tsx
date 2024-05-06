6b359963b8829e27f484199ba16e986f27f1e26a
pervej0 <mdparvez222khan@gmail.com>
2024-05-06 20:40:59 +0600

    axiosBaseQuery added

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
-  return <div></div>;
+  return <>{children}</>;
 };
 
 export default layout;
